63ca85649bfbda30d6927e177fcae2c034cbdc46
unknown <tafaramachingura@gmail.com>
2026-01-06 14:23:59 +0200

    commit

--- warranty_core/settings.py
+++ warranty_core/settings.py
@@ -110,7 +110,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "/register/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
